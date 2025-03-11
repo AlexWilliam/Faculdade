7f8902898a4e285fc9135dc9725a28de20bb3bd0
AlexWilliam <alexgoncalves.ti@gmail.com>
2025-03-11 19:30:50 -0300

    feat: adiciona aula restante

---
+++ Faculdade/CadastroAlunos.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int idade, matricula;
+    float altura;
+    char nome[50];
+
+    printf("Digite seu nome: \n");
+    scanf("%48[^\n]s", nome);
+
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+
+    printf("Digite sua altura: \n");
+    scanf("%f", &altura);
+
+    printf("Digite sua matricula: \n");
+    scanf("%d", &matricula);
+
+    printf("Nome do Aluno: %s - Matrícula: %d\n", nome, matricula);
+    printf("Idade do Aluno: %d - Altura: %.2f\n", idade, altura);
+
+    return 0;
+}
